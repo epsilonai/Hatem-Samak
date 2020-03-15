ac9d34ae456ffc51b3925adfd7a10348f19e6d22
hatem samak <hatem.samak@yahoo.com>
2020-03-15 14:52:42 +0200

    modify

--- app.py
+++ app.py
@@ -34,25 +34,32 @@ def predction():
     cur_model=load_model()
     
     #load Data 
+    # feature=request.json()
     feature = request.get_json()
-    lst=feature["data"]
-    l=np.reshape(lst,(1,-1))
-       
-    #Prediction & Calculate Probaility
-    pred=cur_model.predict(l)
-    if pred[0] ==0:
-        clfy="Not Fraudlent"
-        prob = (cur_model.predict_proba(l)[:,0][0]>=.002).astype(int)
+    if len(feature["data"]) ==30:
+        
+        lst=feature["data"][1:]
+        l=np.reshape(lst,(1,-1))
+        
+        #Prediction & Calculate Probaility
+        pred=cur_model.predict(l)
+        if pred[0] ==0:
+            clfy="Not Fraudlent"
+            prob = (cur_model.predict_proba(l)[:,0][0]>=.002).astype(int)
+        else:
+            clfy="Fraudlent"
+            prob = (cur_model.predict_proba(l)[:,1][0]>=.002).astype(int)
+        
+        return  """<h1> Model is Loaded</h1>
+                <h1> </h1>
+                <h1> The Transaction is : {}</h1>
+                <h1> The Confidence is : {} %</h1>""".format(clfy,int(prob*100))
     else:
-        clfy="Fraudlent"
-        prob = (cur_model.predict_proba(l)[:,1][0]>=.002).astype(int)
+        return """<h1>Instance should be json form and first key is the "data"and it's value is list  with 30 items not {} items </h1>
+                <h1> </h1>"
+                <h1> Try Again</h1>""".format(len(feature["data"]))
     
-    
-    return  """<h1> Model is Loaded</h1>
-            <h1> </h1>
-            <h1> The Transaction is : {}</h1>
-            <h1> The Confidence is : {} %</h1>""".format(clfy,int(prob*100))
-    
+    # return jsonify(feature)
     
     # Onther output    
     #return jsonify({
